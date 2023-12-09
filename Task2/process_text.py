4ba6f1d28c6a68e75573e2088098e4c53459aec8
Ali Jedari Heidarzadeh <ali@Alis-MacBook-Air.local>
2023-12-09 15:59:11 +0200

    WORKING

--- Task2/process_text.py
+++ Task2/process_text.py
@@ -1,29 +1,25 @@
 from numpy import nan
 
 def process_text(msg):
+    '''
+    Process message texts
+    '''
     lines = msg.split('\n')
     what, when, where, why = nan, nan, nan, nan
 
     for line in lines:
-        line = line.strip()
-        if "What" in line and what is nan:
-            #self.__output_data.update({"Event": lines.replace("What:", "")})
-            what = line.strip().replace("What:", "")
+        l = line.upper()
+        if "WHAT" in l and what is nan:
+            what = l.replace("WHAT", "")[1:]
 
-        if "When" in line and when is nan:
-            #self.__output_data.update({"Date": lines.replace("When:", "")})
-            when = line.strip().replace("When:", "")
+        if "WHEN" in l and when is nan:
+            when = l.replace("WHEN", "")
 
-        if "Where" in line and where is nan:
-            #self.__output_data.update({"Location": lines.replace("Where:", "")})
-            where = line.strip().replace("Where:", "")
+        if "WHERE" in l and where is nan:
+            where = l.replace("WHERE", "")
 
-        if "Why" in line and why is nan:
-            #self.__output_data.update({"Why": lines.replace("Why:", "")})
-            why = line.strip().replace("Why:", "")
+        if "WHY" in l and why is nan:
+            why = l.replace("WHY", "")
 
+    print(f'what: {what}, when: {when}, where: ')
     return {'What': [what], 'When': [when], 'Where': [where], 'Why': [why]}
-
-    '''if len(self.__output_data) > 0:
-        for question, text in self.__output_data.items():
-            print(question,":",text,  file = self.__fileWrite)'''
