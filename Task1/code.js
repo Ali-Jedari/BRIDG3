45713292e62cb5c9bbcdefa0f206b7b72033667d
connorbeheydt <connor.beheydt@tuni.fi>
2023-12-09 10:27:00 +0200

    refactored to post table to generic id

--- Task1/code.js
+++ Task1/code.js
@@ -13,20 +13,25 @@ class Course {
     }
 }
 
-function post_table(course_list) {
-    add_header_row("Result #", "Course", "Rating", "Number of Ratings")
+function post_table(course_list, table_id) {
+    add_header_row(table_id,
+                   "Result #", 
+                   "Course", 
+                   "Rating", 
+                   "Number of Ratings")
     for(let i = 0; i < course_list.length; i++) {
-        add_row(i+1,
+        add_row(table_id,
+                i+1,
                 course_list[i].name, 
                 course_list[i].rating, 
                 course_list[i].num_ratings)
     }
 }
 
-function add_row() {
-    let table = document.getElementById("course-table")
+function add_row(table_id) {
+    let table = document.getElementById(table_id)
     let new_row = document.createElement("tr")
-    for (var i = 0; i < arguments.length; i++) {
+    for (var i = 1; i < arguments.length; i++) {
         let new_entry = document.createElement("td")
         new_entry.innerHTML = arguments[i]
         new_row.appendChild(new_entry)
@@ -34,10 +39,10 @@ function add_row() {
     table.appendChild(new_row)
 }
 
-function add_header_row() {
-    let table = document.getElementById("course-table")
+function add_header_row(table_id) {
+    let table = document.getElementById(table_id)
     let new_row = document.createElement("tr")
-    for (var i = 0; i < arguments.length; i++) {
+    for (var i = 1; i < arguments.length; i++) {
         let new_entry = document.createElement("th")
         let new_entry_text = document.createTextNode(arguments[i])
         new_entry.appendChild(new_entry_text)
@@ -46,20 +51,27 @@ function add_header_row() {
     table.appendChild(new_row)
 }
 
+async function update_table(file, table_id) {
+    let json_file = await fetch(file);
+    let json_text = await json_file.text();
+    let json_data = await JSON.parse(json_text)
+    let courses_data = json_data["courses"]
+    let course_list = []
+    for(var i = 0; i < courses_data.length; i++) {
+        course_list.push(
+            new Course (
+                courses_data[i]["name"],
+                courses_data[i]["rating"],
+                courses_data[i]["num_ratings"]
+            )
+        )   
+    }
+    post_table(course_list, table_id)
+  }
+  
 function main() {
     console.log("Program Start")
-    //TODO: Get courses from csv file
-    //TODO: Convert csv into JSON
-    //TODO: Write courses to csv file?
-    course_list = []
-    course_list.push(new Course("Economics 100", 1.2, 10))
-    course_list.push(new Course("Mathematics 100", 2.5, 100))
-    course_list.push(new Course("Economics 200", 3.5, 230))
-    course_list.push(new Course("Mathematics 200", 2.3, 321))
-
-    
-    console.log(course_list)
-    post_table(course_list)
+    update_table("./data.json", "course-table")
 }
 
 main()
